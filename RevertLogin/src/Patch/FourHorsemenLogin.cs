90ab97afa8fd07023c3c14543ce1af71918ba784
Loonie <51543683+ItzLoonie@users.noreply.github.com>
2025-02-15 14:17:46 -0500

    me when it doesnt work part 2

--- RevertLogin/src/Patch/FourHorsemenLogin.cs
+++ RevertLogin/src/Patch/FourHorsemenLogin.cs
@@ -2,42 +2,49 @@ using HarmonyLib;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System;
+using System.Reflection;
 
-namespace RevertLogin.Patch;
-
-[HarmonyPatch(typeof(SceneManager), nameof(SceneManager.LoadScene), new Type[] { typeof(string), typeof(LoadSceneMode) })]
-public class LoginScene_Patch
+namespace RevertLogin.Patch
 {
-    [HarmonyPostfix]
-    private static void OnSceneLoad_Postfix(string sceneName, LoadSceneMode mode)
+    [HarmonyPatch]
+    public class LoginScene_Patch
     {
-        if (sceneName != "LoginScene")
+        static MethodBase TargetMethod()
         {
-            return;
+            return AccessTools.Method(typeof(SceneManager), nameof(SceneManager.LoadScene), new Type[] { typeof(string), typeof(LoadSceneMode) });
         }
 
-        Mod.Logger.LogInfo("LoginScene loaded, modifying UI...");
+        [HarmonyPostfix]
+        private static void OnSceneLoad_Postfix(string sceneName, LoadSceneMode mode)
+        {
+            if (sceneName != "LoginScene")
+            {
+                return;
+            }
 
-        var s4LoginAssets = GameObject.Find("LoginScene/Canvas/S4LoginAssets");
-        if (s4LoginAssets != null)
-        {
-            Mod.Logger.LogInfo("Disabling S4LoginAssets...");
-            s4LoginAssets.SetActive(false);
-        }
-        else
-        {
-            Mod.Logger.LogWarning("S4LoginAssets not found!");
-        }
+            Mod.Logger.LogInfo("LoginScene loaded, modifying UI...");
 
-        var loginAssets = GameObject.Find("LoginAssets");
-        if (loginAssets != null)
-        {
-            Mod.Logger.LogInfo("Enabling LoginAssets...");
-            loginAssets.SetActive(true);
-        }
-        else
-        {
-            Mod.Logger.LogWarning("LoginAssets not found!");
+            var s4LoginAssets = GameObject.Find("/Canvas/S4LoginAssets");
+            if (s4LoginAssets != null)
+            {
+                Mod.Logger.LogInfo("Disabling S4LoginAssets...");
+                s4LoginAssets.SetActive(false);
+            }
+            else
+            {
+                Mod.Logger.LogWarning("S4LoginAssets not found!");
+            }
+
+            var loginAssets = GameObject.Find("/Canvas/LoginAssets");
+            if (loginAssets != null)
+            {
+                Mod.Logger.LogInfo("Enabling LoginAssets...");
+                loginAssets.SetActive(true);
+            }
+            else
+            {
+                Mod.Logger.LogWarning("LoginAssets not found!");
+            }
         }
     }
 }
